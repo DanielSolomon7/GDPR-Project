e77be9f94ec9a55776c14381d5b894b65f34a822
Daniel Solomon <danielsolomon@mailfence.com>
2025-04-07 10:46:10 +0100

    Adding S3 storage bucket.

---
+++ terraform/s3.tf
@@ -0,0 +1,9 @@
+resource "aws_s3_bucket" "storage_bucket" {
+  bucket = "ds-storage-bucket-123"
+
+  tags = {
+    Name        = "storage_bucket"
+    Environment = "Dev"
+    Description = "S3 bucket to store data of people details."
+  }
+}
