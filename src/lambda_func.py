5c2824d4ef8a4231b3e9188afe0dcd27420b8e28
Daniel Solomon <danielsolomon@mailfence.com>
2025-04-07 19:27:57 +0100

    Tidy up for lambda_handler.

--- src/lambda_func.py
+++ src/lambda_func.py
@@ -1,7 +1,6 @@
 import boto3
 import pandas as pd
 from io import BytesIO, StringIO
-import os
 
 
 def lambda_handler(event, context):
@@ -12,11 +11,13 @@ def lambda_handler(event, context):
     try:
         s3 = boto3.client("s3")
         file_name = event["file_to_obfuscate"]
-        obfuscated_file_name = f"obfuscated_{file_name}"
+        obfuscated_file_name =  f"obfuscated_{file_name}"
 
         """Get CSV file from the storage bucket"""
-        response = s3.get_object(Bucket="ds-storage-bucket-123", Key=file_name)
-
+        response = s3.get_object(
+                Bucket="ds-storage-bucket-123", Key=file_name
+            )
+        
         """Convert the CSV File to a DataFrame"""
         content = response["Body"].read()
         df = pd.read_csv(BytesIO(content))
@@ -25,34 +26,20 @@ def lambda_handler(event, context):
         for i, row in df.iterrows():
             for column_name in event["pii_fields"]:
                 if column_name in list(df.columns):
-                    df.at[i, column_name] = "***"
+                    df.at[i, column_name] = '***'
                 else:
                     raise ValueError(f"Invalid column name given: {column_name}")
 
         """Create new CSV file from DataFrame"""
-        # df.to_csv(obfuscated_file_name, index=False)
-
         csv_buffer = StringIO()
         df.to_csv(csv_buffer, header=True, index=False)
-        s3.put_object(
-            Bucket="ds-target-bucket-123",
-            Body=csv_buffer.getvalue(),
-            Key=obfuscated_file_name,
-        )
 
-        # """Upload new CSV file to the target bucket"""
-        # s3.upload_file(
-        #         obfuscated_file_name,
-        #         "ds-target-bucket-123",
-        #         obfuscated_file_name
-        #     )
-
-        # """Remove new CSV file from the local repository"""
-        # os.remove(obfuscated_file_name)
+        """Upload new CSV file to the target bucket"""
+        s3.put_object(Bucket="ds-target-bucket-123", Body=csv_buffer.getvalue(), Key=obfuscated_file_name)
 
         return {
             "result": f"File {obfuscated_file_name} has been created and uploaded to the target bucket."
         }
-
+    
     except Exception as e:
-        raise (e)
+        raise(e)
