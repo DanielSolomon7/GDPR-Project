6b01a8650b2dbf816e6d6c998dca45096f31c08d
Daniel Solomon <danielsolomon@mailfence.com>
2025-04-07 12:22:17 +0100

    Adding IAM role policy attachment for S3 GetObject for lambda.

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -17,3 +17,27 @@ resource "aws_iam_role" "role_for_lambda" {
     name = "role_for_lambda"
     assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
+
+## S3 Policy Document
+data "aws_iam_policy_document" "lambda_policy_doc" {
+    statement {
+      effect = "Allow"
+      actions = [
+        "s3:GetObject"
+      ]
+      resources = ["${aws_s3_bucket.storage_bucket.arn}"]
+    }
+}
+
+## S3 Policy
+resource "aws_iam_policy" "lambda_policy" {
+    name_prefix = "lambda"
+    policy = data.aws_iam_policy_document.lambda_policy_doc.json
+
+}
+
+## S3 Attachment
+resource "aws_iam_role_policy_attachment" "lambda_s3_get_attachment" {
+    role = aws_iam_role.role_for_lambda.name
+    policy_arn = resource.aws_iam_policy.lambda_policy.arn
+}
