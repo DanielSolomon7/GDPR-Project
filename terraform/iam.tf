4c05b1637e0eed7ec00e0fed38b518fe735a0928
Daniel Solomon <danielsolomon@mailfence.com>
2025-04-07 12:50:56 +0100

    Adding put policy for lambda to put object in target bucket.

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -18,8 +18,8 @@ resource "aws_iam_role" "role_for_lambda" {
     assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
-## S3 Policy Document
-data "aws_iam_policy_document" "lambda_policy_doc" {
+## S3 Policy Document for Storage Bucket - GetObject
+data "aws_iam_policy_document" "lambda_get_policy_doc" {
     statement {
       effect = "Allow"
       actions = [
@@ -29,15 +29,39 @@ data "aws_iam_policy_document" "lambda_policy_doc" {
     }
 }
 
-## S3 Policy
-resource "aws_iam_policy" "lambda_policy" {
-    name_prefix = "lambda"
-    policy = data.aws_iam_policy_document.lambda_policy_doc.json
+## S3 Policy for Storage Bucket
+resource "aws_iam_policy" "lambda_get_policy" {
+    name_prefix = "lambda-get-"
+    policy = data.aws_iam_policy_document.lambda_get_policy_doc.json
 
 }
 
-## S3 Attachment
+## S3 Get Attachment
 resource "aws_iam_role_policy_attachment" "lambda_s3_get_attachment" {
     role = aws_iam_role.role_for_lambda.name
-    policy_arn = resource.aws_iam_policy.lambda_policy.arn
+    policy_arn = resource.aws_iam_policy.lambda_get_policy.arn
+}
+
+## S3 Policy Document for Target Bucket - PutObject
+data "aws_iam_policy_document" "lambda_put_policy_doc" {
+    statement {
+      effect = "Allow"
+      actions = [
+        "s3:PutObject"
+      ]
+      resources = ["${aws_s3_bucket.target_bucket.arn}"]
+    }
+}
+
+## S3 Policy for Target Bucket
+resource "aws_iam_policy" "lambda_put_policy" {
+    name_prefix = "lambda-put-"
+    policy = data.aws_iam_policy_document.lambda_put_policy_doc.json
+
+}
+
+## S3 Put Attachment
+resource "aws_iam_role_policy_attachment" "lambda_s3_put_attachment" {
+    role = aws_iam_role.role_for_lambda.name
+    policy_arn = resource.aws_iam_policy.lambda_put_policy.arn
 }
