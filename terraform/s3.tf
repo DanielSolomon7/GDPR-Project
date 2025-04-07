27005bb71ee678e69aeaf1c30d65c76f9fcbd0f5
Daniel Solomon <danielsolomon@mailfence.com>
2025-04-07 12:40:44 +0100

    Adding S3 target bucket.

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -8,3 +8,13 @@ resource "aws_s3_bucket" "storage_bucket" {
     Description = "S3 bucket to store data of people details."
   }
 }
+
+resource "aws_s3_bucket" "target_bucket" {
+  bucket = "ds-target-bucket-123"
+
+  tags = {
+    Name        = "target_bucket"
+    Environment = "Dev"
+    Description = "S3 bucket to store obfuscated data."
+  }
+}
