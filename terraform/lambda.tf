a5950d8bb2047623c54f2647c60f2d289c062f17
Daniel Solomon <danielsolomon@mailfence.com>
2025-04-07 11:27:06 +0100

    Adding Lambda with IAM role.

---
+++ terraform/lambda.tf
@@ -0,0 +1,15 @@
+data "archive_file" "lambda_code" {
+    type = "zip"
+    source_file = "${path.module}/../src/lambda_func.py"
+    output_path = "compressed_lambda_file.zip"
+}
+
+resource "aws_lambda_function" "lambda_func" {
+    filename = "compressed_lambda_file.zip"
+    function_name = "lambda_func"
+    role = aws_iam_role.role_for_lambda.arn
+    handler = "lambda_func.lambda_handler"
+    timeout = 30
+    source_code_hash = data.archive_file.lambda_code.output_base64sha256
+    runtime = "python3.11"
+}
