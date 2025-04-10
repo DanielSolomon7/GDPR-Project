7717841aa327631264ca3ffb657073945e5ba1d0
Daniel Solomon <danielsolomon@mailfence.com>
2025-04-10 18:39:03 +0100

    Further work on obfuscate function.

--- src/obfuscate.py
+++ src/obfuscate.py
@@ -1,6 +1,23 @@
 import boto3
+import json
 
-def obfuscate():
+def obfuscate(json_string):
     # list_state_machines()['stateMachines'] to get the arn
     # .start_execution(arn, input='{"json": "string"}') to run the state machine
-    pass
+    json_dict = json.loads(json_string)
+    
+    arn = None
+    client = boto3.client("stepfunctions")
+    response = client.list_state_machines()['stateMachines']
+    for state_machine in response:
+        if state_machine["name"] == "state-machine-for-lambda":
+            arn = state_machine["stateMachineArn"]
+
+    print(arn)
+
+    response = client.start_execution(stateMachineArn=arn)
+
+    print(response)
+    
+    
+    return {"result": "success"}
