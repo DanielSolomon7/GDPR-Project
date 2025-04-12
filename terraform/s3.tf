2a348f705e2bf1694e6eaf2a2ffb2f9e0f52533c
Daniel Solomon <danielsolomon@mailfence.com>
2025-04-12 19:39:35 +0100

    Removing commented code from s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -19,11 +19,3 @@ resource "aws_s3_bucket" "target_bucket" {
     Description = "S3 bucket to store obfuscated data."
   }
 }
-
-## Upload people_data.csv File to Storage Bucket
-# resource "aws_s3_object" "people_data_csv_file_upload" {
-#   bucket = "ds-storage-bucket-123"
-#   key    = "people_data.csv"
-#   source      = "${path.module}/../test/people_data.csv"
-#   source_hash = filemd5("${path.module}/../test/people_data.csv")
-# }
